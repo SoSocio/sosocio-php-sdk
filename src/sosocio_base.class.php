79e9037980352c244a7a8a4b74b65410051a5415
Frank van Os <frank@sosocio.com>
2015-03-27 13:06:32 +0100

    CHANGED error message

--- src/sosocio_base.class.php
+++ src/sosocio_base.class.php
@@ -99,7 +99,7 @@ class sosocio_base{
 		$curlInfo = curl_getinfo($ch);
 
 		if($curlError = curl_error($ch)){
-			throw new Exception($curlError . $curlInfo['url']);
+			throw new Exception($curlError . ' returned at ' . $curlInfo['url']);
 		}
 
 		if(!in_array($curlInfo['http_code'],$this->httpCodes)){
@@ -111,7 +111,7 @@ class sosocio_base{
 				);
 			}
 			
-			throw new \Exception($code.': '.$result. 'url:'.$curlInfo['url']);
+			throw new \Exception('HTTP status code '.$code.' returned at '.$curlInfo['url'].' | response: '.$result);
 			exit;
 		}
 	}
