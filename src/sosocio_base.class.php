4bace870300ba82edf3b86634fc3cea53dd25bfc
Bjorn Bos <bjorn.bos@sosocio.com>
2019-09-20 17:10:25 +0200

    Changed headers to lowercase

--- src/sosocio_base.class.php
+++ src/sosocio_base.class.php
@@ -9,7 +9,7 @@ if (!function_exists('json_decode') || !function_exists('json_encode')) {
 }
 
 class sosocio_base{
-	
+
 	# API endpoint
 	protected $serverUrl;
 	# API user key
@@ -20,36 +20,36 @@ class sosocio_base{
 	protected $bundleCertificate;
 	# When debugging is enabled, no error is thrown when API call fails
 	protected $debug = FALSE;
-	
-	# Mimetype for requests	
+
+	# Mimetype for requests
 	protected $mimeType = 'application/json';
-	
+
 	# Curl timeout
 	protected $curlTimeOut = 60;
 
 	# Total record count api
 	public $totalRecords;
-	
+
 	# Pagination api
 	public $pagination;
-	
+
 	private $responseHeaders;
-	
+
 	# Request data
 	private $arrData;
-	
+
 	# Default request parameters
 	private $defaultData = 	array(
 		'options'	=>	array(),
 		'inputdata'	=>	array()
 	);
-	
+
 	private $result;
 	protected $error;
 
 	/**
 	 * Get default curl options for request
-	 * 
+	 *
 	 * @return array curl options
 	 */
 	public function getDefaultCurlOptions(){
@@ -66,9 +66,9 @@ class sosocio_base{
 
 	/**
 	 * Build URL for API request
-	 * 
+	 *
 	 * @param str $url: the request endpoint
-	 * 
+	 *
 	 * @return str $finalUrl
 	 */
 	public function buildUrl($url){
@@ -76,23 +76,23 @@ class sosocio_base{
 		$finalUrl  = $this->serverUrl;
 		# Analyze request endpoint
 		$urlParts = parse_url($url);
-	
+
 		# Add request endpoint path to final URL
 		if(isset($urlParts['path']) && !empty($urlParts['path'])){
 			$finalUrl .= $urlParts['path'];
 		}
-		
+
 		if(isset($urlParts['query'])){
-			# Parse the query string 
+			# Parse the query string
 			parse_str($urlParts['query'],$arrQueryParts);
-			
+
 			$finalUrl .= '?'.http_build_query($arrQueryParts);
 		}
-		
+
 		# Return final url
 		return $finalUrl;
 	}
-	
+
 	private function handleError($curlInfo, $result) {
 		if($curlInfo['http_code'] < 200 || $curlInfo['http_code'] >= 300){
 			$code = $curlInfo['http_code'];
@@ -102,32 +102,32 @@ class sosocio_base{
 					'text' => trim($result)
 				);
 			}
-			
+
 			if(!$this->debug) {
 				throw new \Exception($result);
 				exit;
 			}
 		}
 	}
-	
+
 	/**
 	 * Set Curl options for request
-	 * 
+	 *
 	 * @return array curl options
 	 */
 	private function setCurlOptions() {
 
 		# Get default curl options
 		$curlOptions = $this->getDefaultCurlOptions();
-		
+
 		# Set method of curl request
 		$curlOptions[CURLOPT_CUSTOMREQUEST] = strtoupper($this->arrData['method']);
-	
+
 		if(isset($this->arrData['inputdata']['files'])) {
-		
+
 			# Get files from api request
 			$files = $this->arrData['inputdata']['files'];
-	
+
 			# If there are files to be uploaded, add these to curl post fields
 			if(count($files)){
 				$i = 0;
@@ -150,41 +150,41 @@ class sosocio_base{
 		# Return curl options
 		return $curlOptions;
 	}
-	
+
 	private function getCurlValue($filename, $contentType, $postname) {
 	    // PHP 5.5 introduced a CurlFile object that deprecates the old @filename syntax
 	    // See: https://wiki.php.net/rfc/curl-file-upload
 	    if (function_exists('curl_file_create')) {
 	        return curl_file_create($filename, $contentType, $postname);
 	    }
-	
+
 	    // Use the old style if using an older version of PHP
 	    $value = "@{$filename};filename=" . $postname;
 	    if ($contentType) {
 	        $value .= ';type=' . $contentType;
 	    }
-	
+
 	    return $value;
 	}
-	
+
 	/**
 	 * Make API request
-	 * 
+	 *
 	 * @param str url: the API request endpoint
 	 * @param array data: the API request data
-	 * 
+	 *
 	 * @return array result: the result set from the API request
 	 */
 	public function makeRequest($url,$data=array()){
 		# Set API request data (combine default data with request data)
 		$this->arrData = array_merge($this->defaultData, $data);
-		
+
 		# Build URL for request
 		$url = $this->buildUrl($url);
-		
+
 		# Execute API request
 		$this->executeCurl($url);
-	
+
 		# Return results
 	    return $this->result;
 	}
@@ -195,29 +195,29 @@ class sosocio_base{
 
         foreach ($explodedHeaders as $i => $h) {
             $h = explode(':', $h, 2);
-           
+
             if (isset($h[1])) {
                 $headers[$h[0]] = trim($h[1]);
             }
         }
 
 		$this->pagination = array(
-			'previous' => isset($headers['X-Pagination-Previous']) ? $headers['X-Pagination-Previous'] : false,
-			'next' => isset($headers['X-Pagination-Next']) ? $headers['X-Pagination-Next'] : false,
+			'previous' => isset($headers['x-pagination-previous']) ? $headers['x-pagination-previous'] : false,
+			'next' => isset($headers['x-pagination-next']) ? $headers['x-pagination-next'] : false,
 		);
-		
-		$this->totalRecords = isset($headers['X-Total-Records']) ? $headers['X-Total-Records'] : false;
+
+		$this->totalRecords = isset($headers['x-total-records']) ? $headers['x-total-records'] : false;
 
 	}
-	
+
 	/**
 	 * Execute Curl request
-	 * 
+	 *
 	 * @param str url
-	 * 
+	 *
 	 */
 	private function executeCurl($url){
-		
+
 		# Initialize Curl request
 		$ch = curl_init($url);
 
@@ -227,12 +227,12 @@ class sosocio_base{
 
 		# Execute curl and store result in class result property
 		$curlResponse = curl_exec($ch);
-		
+
 		$header_size 			= curl_getinfo($ch, CURLINFO_HEADER_SIZE);
 		$this->responseHeaders 	= substr($curlResponse, 0, $header_size);
 		$result 				= substr($curlResponse, $header_size);
 
-		# Format headers		
+		# Format headers
 	    $this->formatResponseHeaders();
 
 		# Get curl info
@@ -242,10 +242,10 @@ class sosocio_base{
 	    if($curlError = curl_error($ch) && !$this->debug){
 			throw new Exception($curlError . ' returned at ' . $curlInfo['url']);
 		}
-	    
+
 		# Checks for http codes
 	    $this->handleError($curlInfo, $result);
-	    
+
 	    switch($this->mimeType){
 			case 'text/csv':
 				$this->result = $result;
@@ -263,23 +263,23 @@ class sosocio_base{
 		# Close curl request
 	    curl_close($ch);
 	}
-	
+
 	/**
 	* Add where conditions
-	* 
+	*
 	* @param string $url
 	* @param array $conditions
 	*/
 	protected function addConditions($url, $conditions){
-		
+
 		# If no conditions, return the url with all its conditions
 		if(!is_array($conditions)){
 			return $url;
 		}
-		
+
 		# Parse the url so we can get all conditions
 		$urlParts = parse_url($url);
-				
+
 		# Convert url conditions to array
 		if(isset($urlParts['path'])) {
 			# The endpoint of the API call without the extra conditions in the url
@@ -288,33 +288,33 @@ class sosocio_base{
 		else {
 			throw new Exception('Please provide an URL endpoint in SDK call');
 		}
-		
+
 		# Encode where conditions
 		if(isset($conditions['where'])) {
 			$conditions['where'] = json_encode($conditions['where']);
 		}
-		
+
 		# Encode search conditions
 		if(isset($conditions['search'])) {
 			$conditions['search'] = json_encode($conditions['search']);
 		}
-		
+
 		# If there are extra conditions in the url, convert these
 		if(isset($urlParts['query'])){
 			# Parse the query string and store the (extra) conditions in $urlConditions
 			parse_str($urlParts['query'],$urlConditions);
-			
+
 			# Debugging info: SDK users cannot provide the same conditions via URL and SDK call
 			foreach($urlConditions as $paramName => $paramValue){
 				if(array_key_exists($paramName, $conditions)){
 					throw new Exception('Either provide '.$paramName.' parameter via url, or via SDK, cannot have both');
 				}
 			}
-			
+
 			# Merge SDK conditions and URL conditions
 			$conditions = array_merge($conditions, $urlConditions);
 		}
-		
+
 		return $urlEndpoint . '?'.http_build_query($conditions);
 	}
 
